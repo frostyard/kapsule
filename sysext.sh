f766af1c5feb9bdb6451bb1dd44ac3033ec089ac
Lasath Fernando <devel@lasath.org>
2026-01-28 00:40:06 -0600

    update sysext.sh to include systemd-sysext refresh after pulling tar

--- sysext.sh
+++ sysext.sh
@@ -12,4 +12,6 @@ tar -cf ${serve_dir}/kapsule.tar -C ~/src/kde/sysext/kapsule .
 python -m http.server --directory "$serve_dir" 8000 &
 trap "kill $!" EXIT
 
-ssh root@192.168.100.157 importctl pull-tar --class=sysext --verify=no --force http://192.168.100.1:8000/kapsule.tar
+ssh root@192.168.100.157 \
+    "importctl pull-tar --class=sysext --verify=no --force http://192.168.100.1:8000/kapsule.tar && \
+     systemd-sysext refresh"
